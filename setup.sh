a3e30eb8683e97f0cc610473d50100d0dcc669f1
Jesse Gumz <jesse@jwplayer.com>
2017-02-26 23:53:05 -0500

    chmod setup script, add install command

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 mkdir -p bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# install all plugins
+vim +PluginInstall +qall
+
 # symlink ~/.vimrc to ~/.vim/.vimrc
 ln -s ~/.vim/.vimrc ~/.vimrc
